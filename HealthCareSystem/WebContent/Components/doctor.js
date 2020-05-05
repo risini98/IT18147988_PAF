9ee8e4972376019994183eaebda6501309387e04
risini98 <it18147988@my.sliit.lk>
2020-05-05 15:20:41 +0530

    java script was modified

--- HealthCareSystem/WebContent/Components/doctor.js
+++ HealthCareSystem/WebContent/Components/doctor.js
@@ -22,8 +22,49 @@ $(document).on("click", "#btnSave", function(event) {
    		return;
     }
     // If valid----------------------------------
-    $("#formDoctor").submit();
+    var type = ($("#hidDoctorIDSave").val() == "") ? "POST" : "PUT";
+    $.ajax(
+    	{
+    		url : "DoctorAPI",
+    		type : type,
+    		data : $("#formDoctor").serialize(),
+    		dataType : "text",
+    		complete : function(response, status)
+    		{
+    		onDoctorSaveComplete(response.responseText, status);
+    		}
+    	});
 });
+
+function onDoctorSaveComplete(response, status)
+{
+	if(status == "success" ){
+		var resultSet =JSON.parse(response);
+		
+		if (resultSet.status.trim() == "success")
+			{
+				$("#alertSuccess").text("Successfully saved.");
+				$("#alertSuccess").show();
+				
+				$("#divDoctorGrid").html(resultSet.data);
+			}else if (resultSet.status.trim() == "error")
+			{
+				$("#alertError").text(resultSet.data);
+				$("#alertError").show();
+			}
+	}else if (status == "error")
+			{
+				$("#alertError").text("Error while saving.");
+				$("#alertError").show();
+			} else
+			{
+				$("#alertError").text("Unknown error while saving..");
+				$("#alertError").show();
+			}
+		$("#hidDoctorIDSave").val("");
+		$("#formDoctor")[0].reset();
+		
+}
 // UPDATE==========================================
 $(document).on("click", ".btnUpdate", function(event) {
     $("#hidDoctorIDSave").val($(this).closest("tr").find('#hidDoctorIDUpdate').val());
@@ -32,6 +73,47 @@ $(document).on("click", ".btnUpdate", function(event) {
     $("#mail").val($(this).closest("tr").find('td:eq(2)').text());
     $("#speciality").val($(this).closest("tr").find('td:eq(3)').text());
 });
+//REMOVE==================================================
+$(document).on("click", ".btnRemove", function(event)
+		{
+		$.ajax(
+		{
+		url : "DoctorAPI",
+		type : "DELETE",
+		data : "doctorID=" + $(this).data("doctorid"),
+		dataType : "text",
+		complete : function(response, status)
+		{
+		onDoctorDeleteComplete(response.responseText, status);
+		}
+	});
+});
+
+function onDoctorDeleteComplete(response, status)
+{
+if (status == "success")
+	{
+		var resultSet = JSON.parse(response);
+		if (resultSet.status.trim() == "success")
+			{
+				$("#alertSuccess").text("Successfully deleted.");
+				$("#alertSuccess").show();
+				$("#divDoctorGrid").html(resultSet.data);
+			} else if (resultSet.status.trim() == "error")
+			{
+				$("#alertError").text(resultSet.data);
+				$("#alertError").show();
+			}
+	} else if (status == "error")
+	{
+		$("#alertError").text("Error while deleting.");
+		$("#alertError").show();
+	} else
+	{
+		$("#alertError").text("Unknown error while deleting..");
+		$("#alertError").show();
+	}
+}
 // CLIENTMODEL=========================================================================
 function validateDoctorForm() {
 
